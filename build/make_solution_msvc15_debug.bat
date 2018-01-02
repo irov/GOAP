42afbdf3f8608bafead889532f8f91b44e66c2c2
irov <irov13@mail.ru>
2018-01-02 13:39:50 +0200

    fix build

--- build/make_solution_msvc15_debug.bat
+++ build/make_solution_msvc15_debug.bat
@@ -2,14 +2,13 @@
 
 set "build_dir=build"
 set "configuration=Debug"
-set "CMAKE_EXE=%CD%\..\..\cmake\bin\cmake.exe"
 set "SOLUTION_DIR=solutions\GOAP_msvc15_debug"
 
 @pushd ..
 @call vcvarsall_msvc15
 @mkdir %SOLUTION_DIR%
 @pushd %SOLUTION_DIR%
-%CMAKE_EXE% -G "Visual Studio 15 2017" "%CD%\..\.." -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_BUILD_TYPE:STRING='Debug' -DLIBGOAP_SANDBOX:BOOL=TRUE
+CMake -G "Visual Studio 15 2017" "%CD%\..\.." -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_BUILD_TYPE:STRING='Debug' -DLIBGOAP_SANDBOX:BOOL=TRUE
 @popd
 @popd
 
