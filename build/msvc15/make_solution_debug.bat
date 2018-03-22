67ad58a2452377db5368d4880175b6db738d0727
irov <irov13@mail.ru>
2018-03-22 13:46:13 +0200

    spring cleaning

---
+++ build/msvc15/make_solution_debug.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "CONFIGURATION=Debug"
+set "SOLUTION_DIR=..\solutions\GOAP_msvc15_%CONFIGURATION%"
+
+@pushd ..
+@mkdir %SOLUTION_DIR%
+@pushd %SOLUTION_DIR%
+CMake -G "Visual Studio 15 2017" "%CD%\..\.." -DCMAKE_CONFIGURATION_TYPES:STRING=%CONFIGURATION% -DCMAKE_BUILD_TYPE:STRING=%CONFIGURATION% -DLIBGOAP_SANDBOX:BOOL=TRUE
+@popd
+@popd
+
+@echo on
+@pause
